bfe8ba5cc082aeed9e956e2b2fda2ede9d8ea76e
BSKOM <b.shelestua@gmail.com>
2025-01-31 20:29:24 +0000

    feat: add data.js module

---
+++ src/js/data.js
@@ -0,0 +1,13 @@
+// data structure 
+// "ID": N,  "time STAMP" : N,  "posX": N, "posY": N, "posZ": N 
+// this files contains the data points for emulation date from the sensors
+// minimmum 4 columns are required count starts from 0 to N-199
+// ID is the unique identifier for each data point
+const data = [
+    [1, 0, 0, 0, 25],
+    [2, 0, 0, 0, 20],
+    [3, 0, 0, 0, 22],
+    [4, 0, 0, 0, 18],
+];
+
+export default data;
