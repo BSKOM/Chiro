8e36c9894a140e2a70982fc296c37782f7768081
BSKOM <b.shelestua@gmail.com>
2025-02-07 12:59:54 +0000

    fix: general fix displayed error

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import image from './images/lazy.png';
+// import image from './images/lazy.png';
+import App from './js/app';
 
-const createImage = (src: string) => new Promise<HTMLImageElement>((res, rej) => {
-  const img = new Image();
-  img.onload = () => res(img);
-  img.onerror = rej;
-  img.src = src;
-});
+// const createImage = (src: string) => new Promise<HTMLImageElement>((res, rej) => {
+//   const img = new Image();
+//   img.onload = () => res(img);
+//   img.onerror = rej;
+//   img.src = src;
+// });
+// const  async function render() {
+//   const subHeader = document.createElement('h2');
+//   subHeader.innerHTML = 'This elements was created by js';
+//   // const myImage = await createImage(image);
+//   document.body.appendChild(subHeader);
+//   // document.body.appendChild(myImage);
+// }
+App();
 
-async function render() {
-  const subHeader = document.createElement('h2');
-  subHeader.innerHTML = 'This elements was created by js';
-  const myImage = await createImage(image);
-  document.body.appendChild(subHeader);
-  document.body.appendChild(myImage);
-}
-
-render();
+// render();
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 import conArr from "./generator";
 import { drawCanvas } from "./show";
+const relief = conArr;
+
 console.log('Hello i m app.js');
-const relief = conArr();
-drawCanvas(relief);
+export default function App() {
+    drawCanvas(relief);
+
+    // return (
+    //     <div>
+    //         <h1>Hello i m App</h1>
+    //     </div>
+    // );
+}
+
+
 console.log('After drawCanvas');
